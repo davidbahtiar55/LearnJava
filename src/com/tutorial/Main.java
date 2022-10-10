f19cc0f74124c379d1ed94386dbe2f9cc9000211
david bahtiar <davidbahtiar55@gmail.com>
2022-10-10 11:21:32 +0700

    Commit(Pertemuan 4 Done) (+)Add OtongSurotong.java

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello dulur-dulur");
+        System.out.println("baris ke-2 : hai akang pukis");
+
+        System.out.print("baris ke-3:  ini adalah baris ketiga");
+        System.out.print("baris ke-4:  saya meneruskan baris ketiga");
+
+        System.out.print("baris ke-5:  hallo brodi???");
+        System.out.println("baris ke-6:  hallo men!!!");
+
+        System.out.print("baris ke -7 : hari bro ngops tu \n");
+        System.out.print("baris ke -8 : share lok lah \n");
+        System.out.print("baris ke -9 : skuy sokin!!!\n");
+
+        System.out.printf("nagaa langit %d", 212);
+    }
+}
